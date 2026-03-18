314760cec50d1f908ff3278e2fa280c4d6e1dc54
layaanilkumar <106256982+layaanilkumar@users.noreply.github.com>
2026-03-17 23:46:51 -0400

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,24 @@
+from textblob import TextBlob
+import pandas as pd
+
+# Load dataset
+data = pd.read_csv("sentiment_data.csv")
+
+# Function for sentiment
+def get_sentiment(text):
+    analysis = TextBlob(text)
+    if analysis.sentiment.polarity > 0:
+        return "Positive"
+    elif analysis.sentiment.polarity < 0:
+        return "Negative"
+    else:
+        return "Neutral"
+
+# Apply sentiment
+data["sentiment"] = data["text"].apply(get_sentiment)
+
+# Show output
+print(data)
+
+# Save output
+data.to_csv("output.csv", index=False)
